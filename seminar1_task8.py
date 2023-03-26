65f5b7b8fc98f042bec2f92466e740ed31a432d4
vbaidalov <vsb-post@yandex.ru>
2023-03-26 12:03:12 +0300

    task seminar #4 end

--- seminar1_task8.py
+++ seminar1_task8.py
@@ -1,9 +1,27 @@
-#Задача 8: Требуется определить, можно ли от шоколадки размером n × m долек отломить k долек,
-#если разрешается сделать один разлом по прямой между дольками (то есть разломить шоколадку на два прямоугольника).
-#3 2 4 -> yes
-#3 2 1 -> no
-n,m,k = int(input("Первая сторона: ")),int(input("Вторая сторона: ")),int(input("Кол-во долек: "))
-if k%n == 0 or k%m == 0:
-    print(n,m,k, "-> yes")
-else:
-    print(n,m,k, "-> no")
+# Пользователь вводит одно число N – количество арбузов. Вторая строка содержит N чисел,
+# записанных на новой строчке каждое. Здесь каждое число – это масса соответствующего арбуза
+# Input: 5 -> 5 1 6 5 9
+# Output: 1 9
+
+N = int(input("Введите кол-во арбузов: "))
+
+import random
+watermelon = random.sample(range(1, 20), N)
+print(watermelon)
+watermelonMAX = watermelon[0]
+watermelonMIN = watermelon[0]
+
+for i in range(len(watermelon)):
+    if watermelon[i] > watermelonMAX: watermelonMAX = watermelon[i]
+for i in range(len(watermelon)):
+    if watermelon[i] < watermelonMIN: watermelonMIN = watermelon[i]
+
+print(watermelonMAX)
+print(watermelonMIN)
+
+# a=[1,3,6,8,3,23,6,8,5,3,45,6,8,5,3]
+# max = a[0]
+# pos = 0
+# for i in range(len(a)):
+#     if a[i]>max: max=a[i];pos=i
+# print ("max=",max,", pos=",pos)
