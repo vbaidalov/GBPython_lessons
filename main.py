b8e9a10c8ad18ed5de2e55ef45b60974491551ac
vbaidalov <vsb-post@yandex.ru>
2023-04-05 19:07:05 +0300

    seminar 7

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-# Задача 28: Напишите рекурсивную функцию sum(a, b),
-# возвращающую сумму двух целых неотрицательных чисел.
-# Из всех арифметических операций допускаются только +1 и -1. Также нельзя использовать циклы.
-#
-# 2 2
-# 4
 
-a = int(input("Введите первое число А: "))
-b = int(input("Введите второе число В: "))
 
-def sum(a, b):
-    if a == 0:
-        return b;
-    return sum(a - 1, b + 1)
-print(sum(a, b))
+# values = [1, 23, 42, "asdfg"]
+# transformed_values = list(map(lambda x: x))
+# if values == transformed_values:
+#     print("ok")
+# else:
+#     print("fail")
 
 
 
+# transformation = lambda x: x
+
+values = [1, 23, 42, "asdfg"]
+transformed_values = list(map(lambda x: x, values))
+if values == transformed_values: print('ok')
+else: print('fail')
+
+# values = [1, 23, 42, ‘asdfg’]
+# transformed_values = list(map(trasformation, values))
+# if values == transformed_values:
+#     print(‘ok’)
+# else:
+#     print(‘fail’)
